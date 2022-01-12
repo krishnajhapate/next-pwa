cf268cf5ead269bbd2e91cdea9f943433ea0a55c
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-12 11:08:43 +0530

    next pwa complete

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, NextScript, Main } from "next/document";
+import Document, { Html, Head, NextScript, Main } from "next/document";
 
 class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <link ref="manifest" href="/manifest.json" />
-                    <link ref="apple-touch-icon" href="/icon.png" />
-                    <link ref="theme-color" content="#fff" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png" />
+                    <link name="theme-color" content="#fff" />
                 </Head>
                 <body>
                     <Main />
@@ -17,3 +17,5 @@ class MyDocument extends Document {
         );
     }
 }
+
+export default MyDocument;
